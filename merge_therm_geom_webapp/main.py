3461b05e3e8e3e2363e99a70b2e05fee47fddd12
chenkianwee <chenkianwee@gmail.com>
2025-12-05 16:20:14 -0500

    working webapp

---
+++ merge_therm_geom_webapp/main.py
@@ -0,0 +1,174 @@
+import asyncio
+from time import perf_counter
+
+from pyodide.ffi.wrappers import add_event_listener
+from pyscript.ffi import create_proxy
+from pyscript import window, document, PyWorker
+
+from utils import create_hidden_link, write_ply_web, rgb_falsecolors, convertxyz2zxy
+from libthree import get_scene, get_camera, get_renderer, get_orbit_ctrl, get_lights, create_tri_mesh, create_grp, create_cube, viz_pts_color, create_sphere
+
+PROJECTED_PTS = None
+height_3dview_ratio = 0.85
+# get the renderer and append it to index.html
+renderer = get_renderer()
+renderer.setSize(window.innerWidth, window.innerHeight*height_3dview_ratio)
+bottom_side = document.getElementById('bottomSide')
+bottom_side.appendChild(renderer.domElement)
+# get the camera and scene
+camera = get_camera()
+# get the scene
+scene = get_scene()
+# get lights and put in the scene
+lights = get_lights()
+for light in lights:
+    scene.add(light)
+# disable the download button
+dl_btn = document.getElementById("st-download")
+dl_btn.disabled = True
+# orbit controls
+controls = get_orbit_ctrl(camera, renderer)
+
+# create a spinning cube
+init_cube, init_edges = create_cube()
+init_edges.name = 'init_edges'
+scene.add(init_edges)
+
+def set_cam_orig():
+    camera.position.set(1, 1, 4)
+    camera.lookAt(0,0,0)
+
+set_cam_orig()
+
+def animate(*args):
+    controls.update()
+    init_edges.rotation.x += 0.01
+    init_edges.rotation.y += 0.01
+    renderer.render(scene, camera)
+    # Call the animation loop recursively
+    window.requestAnimationFrame(animate_proxy)
+
+animate_proxy = create_proxy(animate)
+window.requestAnimationFrame(animate_proxy)
+
+def change_dialog_text(txt: str):
+    text_elem = document.getElementById("dialogText")
+    text_elem.innerText = txt
+
+def change_color_bar(mnval: float, mxval: float):
+    val_range = mxval - mnval
+    interval = val_range/4
+    intervals = [mnval, mnval+interval, mnval+(interval*2), mnval+(interval*3), mxval]
+    for cnt, i in enumerate(intervals):
+        color_label = document.getElementById("fcval" + str(cnt+1))
+        color_label.textContent = str(round(i, 1))
+
+async def get_bytes_from_file(item) -> bytes:
+    array_buf = await item.arrayBuffer()
+    return array_buf.to_bytes()
+
+async def on_pts_submit(e):
+    try:
+        t1 = perf_counter()
+        submit_btn = document.getElementById("stgeom-submit")
+        submit_btn.disabled = True
+        set_cam_orig()
+        init_edges = scene.getObjectByName( "init_edges", True )
+        scene.add(init_edges)
+        output_p = document.querySelector("#stgeom-output")
+        output_p.textContent = 'Reading ...'
+        st_file_input = document.querySelector("#stpts-file-upload")
+        geom_file_input = document.querySelector("#geom-file-upload")
+        st_file_list = st_file_input.files
+        geom_file_list = geom_file_input.files
+        nstfiles = len(st_file_list)
+        ngfiles = len(geom_file_list)
+        
+        if nstfiles != 0 and ngfiles != 0:
+            # loading dialog to inform users processing in progress
+            loading_dialog = document.getElementById("loading")
+            loading_dialog.showModal()
+            st_item = st_file_list.item(0)
+            geom_item = geom_file_list.item(0)
+
+            st_bytes = await get_bytes_from_file(st_item)
+            geom_bytes = await get_bytes_from_file(geom_item)
+            # get the sensor pos
+            posx_val = float(document.querySelector("#posx").value)
+            posy_val = float(document.querySelector("#posy").value)
+            posz_val = float(document.querySelector("#posz").value)
+            sensor_pos_zxy = convertxyz2zxy([[posx_val, posy_val, posz_val]])[0]
+            # Await for the worker
+            worker_config = {
+                                "packages": ["plyfile>=1.1.3", "geomie3d==0.0.10", "numpy-stl==3.2.0"],
+                                "files": {"./lib/raytracemrtlib.py": "", 
+                                          "./lib/utils.py": ""}
+                            }
+            worker = PyWorker("./worker.py", type="pyodide", config = worker_config)
+            # Await for the worker
+            await worker.ready
+            worker.sync.change_dialog_text = change_dialog_text
+
+            world = create_grp()
+            sensor_pos = [posx_val, posy_val, posz_val]
+            proj_data = await worker.sync.proj_therm2stl(st_bytes, geom_bytes, sensor_pos)
+            stl_xyzs = proj_data.stl
+            ply_xyzs_viz = proj_data.proj_ply_viz
+            ply_write = proj_data.proj_ply_write
+            ply_write = list(map(tuple, ply_write))
+            temps = proj_data.temps
+            cam_pos = proj_data.cam[0]
+            lookat = proj_data.cam[1]
+            # viz the stl file
+            three_mesh, outlines = create_tri_mesh(stl_xyzs)
+            # viz the projected points
+            mn_temp = min(temps)
+            mx_temp = max(temps)
+            change_color_bar(mn_temp, mx_temp)
+            pts_colors = rgb_falsecolors(temps, mn_temp, mx_temp)
+            threejs_pts_proj = viz_pts_color(ply_xyzs_viz, pts_colors, size=0.05)
+            # viz the sensor pos
+            sphere = create_sphere(0.1, 10, 10, r = 1, g = 0, b = 0)
+            sphere.position.set(sensor_pos_zxy[0], sensor_pos_zxy[1], sensor_pos_zxy[2])
+
+            world.add(outlines)
+            world.add(threejs_pts_proj)
+            world.add(sphere)
+            
+            scene.remove(init_edges)
+
+            camera.position.set(cam_pos[0], cam_pos[1], cam_pos[2])
+            camera.lookAt(lookat)
+            scene.add(world)
+            worker.terminate()
+            loading_dialog.close()
+            
+            global PROJECTED_PTS
+            dtype_val = [('x', 'f4'), ('y', 'f4'), ('z', 'f4'), ('temperature', 'f4')]
+            projected_ply = write_ply_web(ply_write, dtype_val)
+            PROJECTED_PTS = projected_ply
+            t2 = perf_counter()
+            dur = int((t2 - t1)/60)
+            if dur == 0:
+                dur = 'less than a minute'
+            output_p.textContent = f"Success! Time Elapsed (mins): {dur}"
+            
+            dl_btn.disabled = False
+            dl_msg = document.querySelector("#st-output")
+            dl_msg.textContent = 'Refresh to do another projection.'
+        else:
+            loading_dialog = document.getElementById("loading")
+            loading_dialog.showModal()
+            change_dialog_text('Please specify STL and PLY file')
+            
+    except Exception as e:
+        change_dialog_text(e)
+        print(e)
+
+def downloadFile(*args):
+    create_hidden_link(PROJECTED_PTS, 'projected_temperatures', 'ply')
+
+if __name__ == "__main__":
+    animate()
+    add_event_listener(document.getElementById("stgeom-submit"), "click", lambda e: asyncio.create_task(on_pts_submit(e)))
+    add_event_listener(document.getElementById("st-download"), "click", downloadFile)
---
+++ merge_therm_geom_webapp/worker.py
@@ -0,0 +1,155 @@
+import geomie3d
+import numpy as np
+
+from raytracemrtlib import separate_rays
+from utils import convertxyz2zxy, read_stl_web, read_ply_web
+from pyscript import sync
+
+def process_plydata(plydata: np.ndarray) -> dict:
+    """
+    separate the plydata into xyzs and temperatures
+
+    Parameters
+    ----------
+    plydata: np.ndarray
+        np.ndarray[shape(n_pts, n_attributes)] 
+
+    Returns
+    -------
+    dict
+        A dictionary containing:
+            - "xyzs": np.ndarray[(n_pts, 3)].
+            - "temps": np.ndarray[(n_pts)].
+    """
+    temps = plydata[:, 3]
+    temp_dicts = []
+    for temp in temps:
+        temp_dicts.append({'temperature': temp})
+
+    vertices = plydata[:, 0:3]
+    return {'xyzs': vertices, 'temps': temp_dicts}
+
+def mesh2pts(mesh_xyzs: np.ndarray):
+    """
+    sort mesh data into points
+
+    Parameters
+    ----------
+    mesh_xyzs : np.ndarray
+        np.ndarray[shape(ntri, 3, 3)] 
+
+    Returns
+    -------
+    np.ndarray
+        np.ndarray[shape(ntri * 3, 3)]
+    """
+    ntri = len(mesh_xyzs)
+    mesh_points = np.reshape(mesh_xyzs, (ntri*3, 3))
+    return mesh_points
+
+def proj_therm2stl(ply_bytes: bytes, stl_bytes: bytes, sensor_pos: list) -> dict:
+    """
+    project thermal point cloud onto stl file
+
+    Parameters
+    ----------
+    ply_bytes: bytes
+        np.ndarray[shape(ntri, 3, 3)] 
+
+    Returns
+    -------
+    flatten_mesh_xyzs : np.ndarray
+        np.ndarray[shape(ntri * 3 * 3)]
+    """
+    #------------------------------------------------------------------
+    # region: read stl file 
+    sync.change_dialog_text('Reading STL file ...')
+    stldata = read_stl_web(stl_bytes)
+    stl_xyzs = stldata['xyzs']
+    tri_ls = []
+    for tri in stl_xyzs:
+        tri_verts = geomie3d.create.vertex_list(tri)
+        tri_face = geomie3d.create.polygon_face_frm_verts(tri_verts)
+        tri_ls.append(tri_face)
+    # endregion: read stl file
+    #------------------------------------------------------------------
+    # region: read ply file
+    sync.change_dialog_text('Reading PLY file ...')
+    plydata = read_ply_web(ply_bytes)
+    plydata = process_plydata(plydata)
+    ply_xyzs = plydata['xyzs']
+    temps = plydata['temps']
+    sync.change_dialog_text('Moving PLY pts to sensor position ...')
+    gverts = geomie3d.create.vertex_list(ply_xyzs, attributes_list=temps)
+    vcomp = geomie3d.create.composite(gverts)
+    mv_vcomp = geomie3d.modify.move_topo(vcomp, sensor_pos)
+    mv_gverts = geomie3d.get.topo_explorer(mv_vcomp, geomie3d.topobj.TopoType.VERTEX)
+    # endregion: read ply file
+    #------------------------------------------------------------------
+    # region: convert the ply data to rays
+    sync.change_dialog_text('Converting PLY pts to rays ...')
+    
+    rays = []
+    for vcnt,v in enumerate(gverts):
+        temp = v.attributes['temperature']
+        ray = geomie3d.create.ray(sensor_pos, v.point.xyz, attributes = {'temperature':temp, 'id': vcnt})
+        rays.append(ray)
+    
+    aloop = 1000000#30
+    ntri = len(stl_xyzs)
+    ndir = len(mv_gverts)
+    ttl = ndir*ntri
+    nparallel = int(ttl/aloop)
+    
+    if nparallel != 0:
+        rays_ls = separate_rays(rays, nparallel)
+    # endregion: convert the ply data to rays
+    #------------------------------------------------------------------
+    # region: project the thermal scan onto stl triangle faces
+    sync.change_dialog_text('Projecting PLY ray onto STL triangles ...')
+    ttl_k = int(ttl/1000)
+    proj_rays = []
+    rcnt = 0
+    for rays1 in rays_ls:
+        rcnt += len(rays1)
+        percentage = int((rcnt*ntri)/ttl * 100)
+        hrs, mrs, hfs, mfs = geomie3d.calculate.rays_faces_intersection(rays1, tri_ls)
+        nhr = len(hrs)
+        nmr = len(mrs)
+        msg = f"Projecting {ndir} PLY ray onto {ntri} STL triangles ... \n{percentage}% of {ttl_k}k calculations completed"
+        msg += f"\n{nhr} rays intersection, {nmr} rays did not hit any surfaces"
+        sync.change_dialog_text(msg)
+        proj_rays.extend(hrs)
+    # endregion: project the thermal scan onto stl triangle faces
+    #------------------------------------------------------------------
+    # region: prepare data
+    # stl triangles
+    stl_pts_xyzs = mesh2pts(stl_xyzs)
+    stl_pts_zxys = convertxyz2zxy(stl_pts_xyzs)
+    stl_pts_zxys_flatten = stl_pts_zxys.flatten().tolist()
+
+    bbox = geomie3d.calculate.bbox_frm_xyzs(stl_pts_xyzs)
+    bbox_arr = bbox.bbox_arr
+    cam_pos = [bbox_arr[3]+5, bbox_arr[4]+5, bbox_arr[5]+5]
+    bbox_midpt = geomie3d.calculate.bboxes_centre([bbox])[0].tolist()
+    cam_place = [cam_pos, bbox_midpt]
+    cam_place_zxy = convertxyz2zxy(cam_place)
+    # ply pts
+    proj_xyzs = []
+    ply_verts = []
+    temps = []
+    for proj_ray in proj_rays:
+        intx = proj_ray.attributes['rays_faces_intersection']['intersection'][0]
+        temp = float(proj_ray.attributes['temperature'])
+        temps.append(temp)
+        ply_verts.append((intx[0], intx[1], intx[2], temp))
+        proj_xyzs.append(intx)
+    
+    proj_xyzs = convertxyz2zxy(proj_xyzs)
+    proj_xyzs_flatten = np.array(proj_xyzs).flatten().tolist()
+    return {'stl': stl_pts_zxys_flatten, 'proj_ply_viz': proj_xyzs_flatten, 
+            'proj_ply_write': ply_verts, 'temps': temps, 'cam': cam_place_zxy}
+    # endregion: prepare data
+    #------------------------------------------------------------------
+
+sync.proj_therm2stl = proj_therm2stl
